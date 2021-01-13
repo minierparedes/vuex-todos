feeea404857884088ab5d73e62557e3240ba5816
minierparedes <3thancr0wn@gmail.com>
2021-01-13 21:30:41 +0900

    Updated: addTodo: store actions: components addTodo.vue.

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -11,17 +11,22 @@ const getters = {
 const actions = {
     async fetchTodos({ commit }) {
         const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
-        commit("setTodos",response.data);
+        commit("setTodos", response.data);
+    },
+    async addTodo({ commit }, title) {
+        const response = await axios.post("https://jsonplaceholder.typicode.com/todos", { title, completed: false });
+        commit("newTodo", response.data)
     }
 };
 
 const mutations = {
-        setTodos: (state, todos) => (state.todos = todos)
-    };
+    setTodos: (state, todos) => (state.todos = todos),
+    newTodo: (state, todo) => state.todos.unshift(todo)
+};
 
-    export default {
-        state,
-        getters,
-        actions,
-        mutations
-    };
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
